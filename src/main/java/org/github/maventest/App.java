1f9275d3d0ab776decaabde65e6a44afb8775995
Lenovo-PC <e999or@bk.ru>
2020-03-01 19:13:19 +0300

    git init

---
+++ src/main/java/org/github/maventest/App.java
@@ -0,0 +1,13 @@
+package org.github.maventest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
